b7684e41619d3a60b12aaf4f3b1d79860f00eb71
Greg Perrin <gregoryperrin.ucb@gmail.com>
2023-12-08 14:48:42 -0700

    LSP fix with indent stuff. :

--- nvim/lua/config/solidity.lua
+++ nvim/lua/config/solidity.lua
@@ -16,13 +16,22 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    solidity_ls_nomicfoundation = function(_, opts)
+      require("lsp.utils").on_attach(function(client, bufnr)
+        if client.name == "solidity_ls_nomicfoundation" then
+          vim.o.tabstop = 1
+          vim.o.shiftwidth = 2
+        end
+      end)
+      return true
+    end,
     opts = {
       servers = {
         solidity_ls_nomicfoundation = {
           setup = {
             cmd = { 'nomicfoundation-solidity-language-server', '--stdio' },
             filetypes = { 'solidity' },
-            -- root_dir = require("lsp.utils").find_git_ancestor,
+            root_dir = require("lsp.utils").find_git_ancestor,
             single_file_support = true,
           },
         },
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -394,4 +394,7 @@ return {
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
+  {
+    'tpope/vim-abolish'
+  },
 }
