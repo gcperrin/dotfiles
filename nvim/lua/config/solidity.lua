4d23c191ace2376b597cb75e3732decdbc836c4f
Greg Perrin <gregoryperrin.ucb@gmail.com>
2023-12-07 23:05:44 -0700

    Fix some issues with LSP...still a little wonky

--- nvim/lua/config/solidity.lua
+++ nvim/lua/config/solidity.lua
@@ -10,8 +10,23 @@ return {
     opts = function(_, opts)
       vim.list_extend(
         opts.ensure_installed,
-        { "solidity" }
+        { "nomicfoundation-solidity-language-server" }
       )
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        solidity_ls_nomicfoundation = {
+          setup = {
+            cmd = { 'nomicfoundation-solidity-language-server', '--stdio' },
+            filetypes = { 'solidity' },
+            -- root_dir = require("lsp.utils").find_git_ancestor,
+            single_file_support = true,
+          },
+        },
+      },
+    },
   },
 }
